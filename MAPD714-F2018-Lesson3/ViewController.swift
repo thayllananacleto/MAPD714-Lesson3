e53a606b9832f0589febb0085e7151230a781863
Thayllan Anacleto <thayllananacleto@gmail.com>
2018-09-20 20:48:43 -0400

    Initial Commit

---
+++ MAPD714-F2018-Lesson3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD714-F2018-Lesson3
+//
+//  Created by Thayllan Anacleto on 2018-09-20.
+//  Copyright © 2018 Thayllan Anacleto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
